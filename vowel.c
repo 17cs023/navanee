9ee0a342189a311d026cd42e5fec6ce989a84c1a
17cs023 <43338010+17cs023@users.noreply.github.com>
2018-09-18 15:44:13 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ char [20];
+ printf("input");
+ scanf("%s",&i);
+ }
+ if(i=alpabet)
+ {
+ printf("\n vowels");
+ }
+ else
+ {
+ printf("consonenet")
+ }
+ getch();
+ 
+ 
+
+       
