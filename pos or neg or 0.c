093ba2b6f584e12a6a61974d657b9af38f159051
17cs023 <43338010+17cs023@users.noreply.github.com>
2018-09-18 13:37:06 +0530

    Create pos or neg or 0.c

---
+++ pos or neg or 0.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+printf("/n input:");
+scanf("%d",&i);
+}
+if(i=num)
+{
+printf("positive");
+elseif(i=-num)
+printf("negative");
+else
+printf("zero");
+}
+getch();
