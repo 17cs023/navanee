83b3b749ebc3dd63fb9501f0f94f0e132f1383ca
17cs023 <43338010+17cs023@users.noreply.github.com>
2019-01-03 14:34:01 +0530

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -2,13 +2,13 @@
 int main()
 {
     char c;
-    printf("Enter a character: ");
+    
     scanf("%c",&c);
 
     if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
-        printf("%c is an alphabet.",c);
+        printf(" alphabet");
     else
-        printf("%c is not an alphabet.",c);
+        printf("no");
 
     return 0;
 }
