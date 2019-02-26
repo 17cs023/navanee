e9d96b5e9c2243bcd678b147bac7d29f912d9299
17cs023 <43338010+17cs023@users.noreply.github.com>
2019-02-26 12:22:42 +0530

    Create product.c

---
+++ product.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,n;
+printf("enter 2 number");
+scanf("%d%d",&a,&b);
+n=a*b;
+if(n%2==0)
+{
+printf("product is even");
+}
+else
+{
+printf("product is odd");
+}
+}
